d8398aa05ce706556e935e0b158a33267da02040
Michael Walelign <bekwab19@gmail.com>
2021-06-27 15:35:33 +0200

    one code added

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 #print(5//4)
 print(round(3.4))
 print(abs(-10))
+print("Hello {}, your balance is {}.".format("Cindy", 50))
+
+print("Hello {0}, your balance is {1}.".format("Cindy", 50))
+
+print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+
+print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
