74a9a5e2839fcdb9efc97807849c0ddd51d7ec19
Michael Walelign <bekwab19@gmail.com>
2021-06-27 15:38:09 +0200

    one code added

--- main.py
+++ main.py
@@ -4,12 +4,19 @@
 # print(2//4)
 # print(3//4)
 #print(5//4)
-print(round(3.4))
-print(abs(-10))
-print("Hello {}, your balance is {}.".format("Cindy", 50))
+# print(round(3.4))
+# print(abs(-10))
+# print("Hello {}, your balance is {}.".format("Cindy", 50))
+#
+# print("Hello {0}, your balance is {1}.".format("Cindy", 50))
+#
+# print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+#
+# print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
 
-print("Hello {0}, your balance is {1}.".format("Cindy", 50))
 
-print("Hello {name}, your balance is {amount}.".format(name="Cindy", amount=50))
+str= "01234567"
 
-print("Hello {0}, your balance is {amount}.".format("Cindy", amount=50))
+#[strat:stop:step over]
+print (str[0:3])
+print (str[0:7:2])
