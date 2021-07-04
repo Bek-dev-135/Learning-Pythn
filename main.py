535e3379d5ceb62ca504e08b4bb478f78d999c91
Michael Walelign <bekwab19@gmail.com>
2021-07-04 12:02:01 +0200

    oop pet exercise

--- main.py
+++ main.py
@@ -330,23 +330,86 @@
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 
-class Cat:
-    species = 'mammal'
+# class Cat:
+#     species = 'mammal'
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#
+# def oldestCat(list):
+#         return max(list)
+#
+# cat1 = Cat('tokyo', 3)
+# cat2 = Cat('simp', 2)
+# cat3 = Cat('toro', 4)
+#
+#
+#
+# cats = [cat1.age, cat2.age, cat3.age]
+#
+#
+# print(f"The oldest cat is {oldestCat(cats)} years old.")
+
+
+# class Cat:
+#     species = 'mammal'
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     @classmethod
+#     def adding(cls, num1, num2):
+#         return cls('ted', num1+num2)
+#
+# pcat3=Cat.adding(2,3)
+#
+# print(pcat3.age)
+
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+class Cat():
+    is_lazy = True
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def walk(self):
+        return f'{self.name} is just walking around'
 
-def oldestCat(list):
-        return max(list)
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-cat1 = Cat('tokyo', 3)
-cat2 = Cat('simp', 2)
-cat3 = Cat('toro', 4)
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
 
-cats = [cat1.age, cat2.age, cat3.age]
+class Garfild(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+simon=Simon('simon', 4)
+sally=Sally('sally', 6)
+garfield=Garfild('garfield', 3)
+my_cats= [simon,sally,garfield]
+
+my_pets= Pets(my_cats)
+my_pets.walk()
+
+#1 Add nother Cat
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
 
 
-print(f"The oldest cat is {oldestCat(cats)} years old.")
+#3 Instantiate the Pet class with all your cats use variable my_pets
 
-
+#4 Output all of the cats walking using the my_pets instance
