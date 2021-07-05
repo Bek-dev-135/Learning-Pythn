81d283c79d990b8292b170bbbe3af96bd99c8578
Michael Walelign <bekwab19@gmail.com>
2021-07-05 23:30:12 +0200

    oop super class

--- main.py
+++ main.py
@@ -366,50 +366,62 @@
 #
 # print(pcat3.age)
 
-class Pets():
-    animals = []
-    def __init__(self, animals):
-        self.animals = animals
+# class Pets():
+#     animals = []
+#     def __init__(self, animals):
+#         self.animals = animals
+#
+#     def walk(self):
+#         for animal in self.animals:
+#             print(animal.walk())
+#
+# class Cat():
+#     is_lazy = True
+#
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def walk(self):
+#         return f'{self.name} is just walking around'
+#
+# class Simon(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+#
+# class Sally(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+#
+# class Garfild(Cat):
+#     def sing(self, sounds):
+#         return f'{sounds}'
+#
+# simon=Simon('simon', 4)
+# sally=Sally('sally', 6)
+# garfield=Garfild('garfield', 3)
+# my_cats= [simon,sally,garfield]
+#
+# my_pets= Pets(my_cats)
+# my_pets.walk()
+#
+# #1 Add nother Cat
+#
+# #2 Create a list of all of the pets (create 3 cat instances from the above)
+#
+#
+# #3 Instantiate the Pet class with all your cats use variable my_pets
+#
+# #4 Output all of the cats walking using the my_pets instance
 
-    def walk(self):
-        for animal in self.animals:
-            print(animal.walk())
-
-class Cat():
-    is_lazy = True
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def walk(self):
-        return f'{self.name} is just walking around'
-
-class Simon(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Sally(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-class Garfild(Cat):
-    def sing(self, sounds):
-        return f'{sounds}'
-
-simon=Simon('simon', 4)
-sally=Sally('sally', 6)
-garfield=Garfild('garfield', 3)
-my_cats= [simon,sally,garfield]
-
-my_pets= Pets(my_cats)
-my_pets.walk()
-
-#1 Add nother Cat
-
-#2 Create a list of all of the pets (create 3 cat instances from the above)
+class SuperList(list):
 
 
-#3 Instantiate the Pet class with all your cats use variable my_pets
 
-#4 Output all of the cats walking using the my_pets instance
+
+
+     def __len__(self):
+        return 1000
+
+super_list=SuperList()
+print(len(super_list))
