c633eb672a324c083b2c1cc996187ac9fb4bdc56
Michael Walelign <bekwab19@gmail.com>
2021-07-07 23:21:00 +0200

    lamda usage

--- main.py
+++ main.py
@@ -448,40 +448,51 @@
 
 
 
-from functools import reduce
-
-#1 Capitalize all of the pet names and print the list
-my_pets = ['sisi', 'bibi', 'titi', 'carla']
-def capitalize(item):
-    return item.upper()
-
-print(list(map(capitalize,my_pets)))
-
-#2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
-my_strings = ['a', 'b', 'c', 'd', 'e']
-my_numbers = [5,4,3,2,1]
-my_numbers.sort()
-
-print (list(zip(my_numbers, my_strings)))
-
-# better to use
+# from functools import reduce
 #
-# print(list(zip(my_strings, sorted(my_numbers))))
+# #1 Capitalize all of the pet names and print the list
+# my_pets = ['sisi', 'bibi', 'titi', 'carla']
+# def capitalize(item):
+#     return item.upper()
 #
-# because sorted() doesnt affect the original list
+# print(list(map(capitalize,my_pets)))
+#
+# #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+# my_strings = ['a', 'b', 'c', 'd', 'e']
+# my_numbers = [5,4,3,2,1]
+# my_numbers.sort()
+#
+# print (list(zip(my_numbers, my_strings)))
+#
+# # better to use
+# #
+# # print(list(zip(my_strings, sorted(my_numbers))))
+# #
+# # because sorted() doesnt affect the original list
+#
+#
+# #3 Filter the scores that pass over 50%
+# scores = [73, 20, 65, 19, 76, 100, 88]
+#
+# def passed(pp):
+#     return pp>50
+#
+# print(list(filter(passed,scores)))
+#
+# #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+#
+# def accumulator(acc, item):
+#     return acc+item
+#
+# print(reduce(accumulator, (my_numbers+ scores)))
+#
+# my_pets = ['sisi', 'bibi', 'titi', 'carla']
+#
+# print(list(map(lambda item: item.upper(), my_pets)))
 
+print(list(map(lambda num: num**2, [1,2,3])))
 
-#3 Filter the scores that pass over 50%
-scores = [73, 20, 65, 19, 76, 100, 88]
+a = [(0, 2), (5, 2), (9, 9), (10, -1)]
+a.sort(key=lambda x: x[1])
 
-def passed(pp):
-    return pp>50
-
-print(list(filter(passed,scores)))
-
-#4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
-
-def accumulator(acc, item):
-    return acc+item
-
-print(reduce(accumulator, (my_numbers+ scores)))
+print(a)
