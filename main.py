86d1e6302bfa609bf8494d718d6d165af9308344
Michael Walelign <bekwab19@gmail.com>
2021-07-10 14:00:18 +0200

    generators create list

--- main.py
+++ main.py
@@ -549,28 +549,59 @@
 #
 # message_friends(user1)
 
-from time import time
-def performance(fn):
-    def wrapper(*args, **kawrgs):
-        t1 = time()
-        result = fn(*args, **kawrgs)
-        t2 = time()
-        print(f'took {t2-t1} s')
-        return result
-    return wrapper
+# from time import time
+# def performance(fn):
+#     def wrapper(*args, **kawrgs):
+#         t1 = time()
+#         result = fn(*args, **kawrgs)
+#         t2 = time()
+#         print(f'took {t2-t1} s')
+#         return result
+#     return wrapper
+#
+# @performance
+# def long_time():
+#     print('1')
+#     for i in range(100000):
+#         i*5
+# @performance
+# def long_time2():
+#     print('2')
+#     for i in list(range(100000)):
+#         i*5
+#
+#
+# long_time()
+# long_time2()
 
-@performance
-def long_time():
-    print('1')
-    for i in range(100000):
-        i*5
-@performance
-def long_time2():
-    print('2')
-    for i in list(range(100000)):
-        i*5
+def special_for(iterable):
+  iterator = iter(iterable)
+  while True:
+    try:
+      iterator*5
+      next(iterator)
+    except StopIteration:
+      break
 
 
-long_time()
-long_time2()
+class MyGen:
+  current = 0
+  def __init__(self, first, last):
+    self.first = first
+    self.last = last
+    MyGen.current = self.first #this line allows us to use the current number as the starting point for the iteration
+
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    if MyGen.current < self.last:
+      num = MyGen.current
+      MyGen.current += 1
+      return num
+    raise StopIteration
+
+gen = MyGen(1,100)
+for i in gen:
+    print(i)
 
