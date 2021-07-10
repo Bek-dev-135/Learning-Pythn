cc4a6ff498a4c947a271615687d0b8779bc90f52
Michael Walelign <bekwab19@gmail.com>
2021-07-10 13:10:24 +0200

    generators time

--- main.py
+++ main.py
@@ -530,22 +530,47 @@
 # long_time()
 
 # Create an @authenticated decorator that only allows the function to run if user1 has 'valid' set to True:
-user1 = {
-    'name': 'Sorna',
-    'valid': False #changing this will either run or not run the message_friends function.
-}
+# user1 = {
+#     'name': 'Sorna',
+#     'valid': False #changing this will either run or not run the message_friends function.
+# }
+#
+# def authenticated(fn):
+#   # code here
+#   def test(x):
+#     if bool(x['valid']) == True:
+#       fn(x)
+#
+#   return test
+#
+# @authenticated
+# def message_friends(user):
+#     print('message has been sent')
+#
+# message_friends(user1)
 
-def authenticated(fn):
-  # code here
-  def test(x):
-    if bool(x['valid']) == True:
-      fn(x)
+from time import time
+def performance(fn):
+    def wrapper(*args, **kawrgs):
+        t1 = time()
+        result = fn(*args, **kawrgs)
+        t2 = time()
+        print(f'took {t2-t1} s')
+        return result
+    return wrapper
 
-  return test
+@performance
+def long_time():
+    print('1')
+    for i in range(100000):
+        i*5
+@performance
+def long_time2():
+    print('2')
+    for i in list(range(100000)):
+        i*5
 
-@authenticated
-def message_friends(user):
-    print('message has been sent')
 
-message_friends(user1)
+long_time()
+long_time2()
 
