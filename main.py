2d7533fe539b0c77b033ce0c4cb682b4cf419331
Michael Walelign <bekwab19@gmail.com>
2021-07-10 15:18:52 +0200

    generators excercise

--- main.py
+++ main.py
@@ -573,35 +573,48 @@
 #
 # long_time()
 # long_time2()
+#
+# def special_for(iterable):
+#   iterator = iter(iterable)
+#   while True:
+#     try:
+#       iterator*5
+#       next(iterator)
+#     except StopIteration:
+#       break
+#
+#
+# class MyGen:
+#   current = 0
+#   def __init__(self, first, last):
+#     self.first = first
+#     self.last = last
+#     MyGen.current = self.first #this line allows us to use the current number as the starting point for the iteration
+#
+#   def __iter__(self):
+#     return self
+#
+#   def __next__(self):
+#     if MyGen.current < self.last:
+#       num = MyGen.current
+#       MyGen.current += 1
+#       return num
+#     raise StopIteration
+#
+# gen = MyGen(1,100)
+# for i in gen:
+#     print(i)
 
-def special_for(iterable):
-  iterator = iter(iterable)
-  while True:
-    try:
-      iterator*5
-      next(iterator)
-    except StopIteration:
-      break
+def fib(n):
+    i = 0
+    j = 1
+
+    for f in range(n):
+        yield i
+        sum= i+j
+        i=j
+        j=sum
 
 
-class MyGen:
-  current = 0
-  def __init__(self, first, last):
-    self.first = first
-    self.last = last
-    MyGen.current = self.first #this line allows us to use the current number as the starting point for the iteration
-
-  def __iter__(self):
-    return self
-
-  def __next__(self):
-    if MyGen.current < self.last:
-      num = MyGen.current
-      MyGen.current += 1
-      return num
-    raise StopIteration
-
-gen = MyGen(1,100)
-for i in gen:
-    print(i)
-
+for x in fib(10):
+    print(x)
