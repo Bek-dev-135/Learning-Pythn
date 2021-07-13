269c392e817688724471929aead9e295a308da52
Michael Walelign <bekwab19@gmail.com>
2021-07-14 00:54:08 +0200

    q6

--- main.py
+++ main.py
@@ -696,15 +696,45 @@
 #
 #     Also please include simple test function to test the class methods.
 
-class Major:
-    def getString(self,stringg):
-        self.stringg= stringg
+# class Major:
+#     def getString(self,stringg):
+#         self.stringg= stringg
+#
+#     def printString(self):
+#        print (self.stringg.upper())
+#
+#
+#
+# s1= Major()
+# s1.getString(str(input('enter a string: ')))
+# s1.printString()
 
-    def printString(self):
-       print (self.stringg.upper())
 
 
+#     Write a program that calculates and prints the value according to the given formula:
+#
+#     Q = Square root of [(2 _ C _ D)/H]
+#
+#     Following are the fixed values of C and H:
+#
+#     C is 50. H is 30.
+#
+#     D is the variable whose values should be input to your program in a comma-separated
+#     sequence.For example Let us assume the following comma separated input sequence is given
+#     to the program:
+#
+# 100,150,180
+#
+#     The output of the program should be:
+#
+# 18,22,24
 
-s1= Major()
-s1.getString(str(input('enter a string: ')))
-s1.printString()
+
+import math
+(c,h)= (50,30)
+string=input('give a list of numbers separated by comma: ')
+
+for item in string.split(','):
+
+    print(round(math.sqrt(2 * c * float(item)/h)),end=', ')
+
