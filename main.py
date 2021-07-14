8a72399ffc79b44657a25ff13855cccf77e8e66f
Michael Walelign <bekwab19@gmail.com>
2021-07-14 21:02:25 +0200

    q7

--- main.py
+++ main.py
@@ -729,12 +729,43 @@
 #
 # 18,22,24
 
+#
+# import math
+# (c,h)= (50,30)
+# string=input('give a list of numbers separated by comma: ')
+#
+# for item in string.split(','):
+#
+#     print(round(math.sqrt(2 * c * float(item)/h)),end=', ')
 
-import math
-(c,h)= (50,30)
-string=input('give a list of numbers separated by comma: ')
+# Question:
+#
+#     _Write a program which takes 2 digits, X,Y as input and generates a
+# 2-dimensional array. The element value in the i-th row and j-th column of the
+# array should be i _ j.*
+#
+#     Note: i=0,1.., X-1; j=0,1,¡­Y-1. Suppose the following inputs are given
+# to the program: 3,5
+#
+#     Then, the output of the program should be:
+#
+# [[0, 0, 0, 0, 0],
+#  [0, 1, 2, 3, 4],
+#  [0, 2, 4, 6, 8]]
+inn=input('input a number: ').split(',')
+i=int(inn[0])
+j=int(inn[1])
+list=[]
 
-for item in string.split(','):
+for item in range(0,i):
+    sub_list = []
+    for tem in range(0,j):
+        sub_list.append(item*tem)
+    list.append(sub_list)
 
-    print(round(math.sqrt(2 * c * float(item)/h)),end=', ')
+print(list)
+
+
+
+
 
