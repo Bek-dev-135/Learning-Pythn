2536476804d6ddca3bc145aa4acc783f4e7a3ac3
Michael Walelign <bekwab19@gmail.com>
2021-08-10 01:24:12 +0200

    pillow

--- 1.py
+++ 1.py
@@ -1,16 +1,37 @@
-import sys
+# import sys
+#
+#
+# import random
+#
+# num=random.randint(1,10)
+#
+# i=int(sys.argv[1])
+# b=True
+# while b:
+#     if i>0 and i<10:
+#
+#         if i==num :
+#             print('you genius')
+#             b=False
+#         else:
+#             print('try again')
+#             b = False
+#     else:
+#         print('enter a number between 1-10')
 
 
-import random
+import unittest
+import main
 
-num=random.randint(1,2)
 
-i=int(sys.argv[1])
-b=True
-while b:
-    if i==num:
-        print('you genius')
-        b=False
-    else:
-        print('try again')
-        b = False
+class TestMain(unittest.TestCase):
+    def tester1(self):
+        answer=5
+        random=5
+        result= main.ranndomize(answer,random)
+        self.assertTrue(result)
+
+
+unittest.main()
+
+
