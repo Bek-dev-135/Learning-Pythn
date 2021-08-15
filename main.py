d43cec51182f11765b9c7e6c364ab0447a9ecd48
Michael Walelign <bekwab19@gmail.com>
2021-08-15 16:12:29 +0200

    q18

--- main.py
+++ main.py
@@ -1293,19 +1293,70 @@
 # 500
 
 import sys
+#
+# from pip._vendor.distlib.compat import raw_input
+#
+# net_amount=0
+# while True:
+#     s=raw_input('hdgh')
+#     if not s:
+#         break
+#     value= s.split(' ')
+#     operation=value[0]
+#     amount=int(value[1])
+#     if operation=='D':
+#         net_amount+=amount
+#     if operation=='W':
+#         net_amount-= amount
+# print(net_amount)
 
-from pip._vendor.distlib.compat import raw_input
 
-net_amount=0
-while True:
-    s=raw_input('hdgh')
-    if not s:
-        break
-    value= s.split(' ')
-    operation=value[0]
-    amount=int(value[1])
-    if operation=='D':
-        net_amount+=amount
-    if operation=='W':
-        net_amount-= amount
-print(net_amount)
+
+
+
+#
+# Question 18
+# Question:
+#
+#     A website requires the users to input username and password to register. Write
+# a program to check the validity of password input by users.
+#
+#     Following are the criteria for checking the password:
+#
+#     At least 1 letter between [a-z]
+#     At least 1 number between [0-9]
+#     At least 1 letter between [A-Z]
+#     At least 1 character from [$#@]
+#     Minimum length of transaction password: 6
+#     Maximum length of transaction password: 12
+#
+#     Your program should accept a sequence of comma separated passwords and will
+# check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+#
+#     Example
+#
+#     If the following passwords are given as input to the program:
+#
+# ABd1234@1,a F1#,2w3E*,2We3345
+#
+#     Then, the output of the program should be:
+#
+# ABd1234@1
+
+
+import re
+
+strng='ABd1234@1,a F1#,2w3E*,2We3345'
+list=strng.split(',')
+posble_pass=[]
+for i in list:
+
+    pattern=re.compile(r"[A-Za-z0-9@#$]{6,12}\d")
+    a=pattern.search(i)
+    check=pattern.fullmatch(i)
+
+    if check!= None:
+        posble_pass.append(i)
+
+for j in posble_pass:
+    print(j)
