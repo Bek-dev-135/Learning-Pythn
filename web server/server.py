95a831bdaa1271acd5e224b8185c52a65f3a109d
Michael Walelign <bekwab19@gmail.com>
2021-11-01 23:42:12 +0100

    simple server2

--- web server/server.py
+++ web server/server.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
+@app.route("/<username>")
+def show_name(username=None):
+    return render_template('index.html', name= username)
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-@app.route("/")
+@app.route("/blog")
 def blog():
     return "<p>These is a blog</p>"
