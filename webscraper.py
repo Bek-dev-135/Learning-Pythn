1a316e17fbf18c5a63b4832b8b91c2d10320dbee
Michael Walelign <bekwab19@gmail.com>
2021-11-01 13:27:36 +0100

    pprint

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from pprint import pprint
 res =requests.get('https://news.ycombinator.com/news')
 # print(res.text)
 soup= BeautifulSoup(res.text, 'html.parser')
@@ -19,16 +19,16 @@ subtexts= soup.select(".subtext")
 
 def create_custom_hn(links,subtexts):
     hn=[]
-    i=0
+
     for idx,item in enumerate(links):
         title= links[idx].getText()
         href=links[idx].get('href', None)
         vote=subtexts[idx].select(".score")
 
-        if len(vote):
+        if len(vote) :
             points=int(vote[0].getText().replace("points", ""))
 
 
             hn.append({'title': title, "link": href, "votes":points})
-    return hn,len(hn),i
-print (create_custom_hn(links,subtexts))
+    return hn
+pprint(create_custom_hn(links,subtexts))
