c6373bf7a86b16d46db803fe169b03fcdb01b1e2
Michael Walelign <bekwab19@gmail.com>
2021-11-01 14:30:21 +0100

    how to loop over and scrape multiple pages

--- webscraper.py
+++ webscraper.py
@@ -1,34 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
-res =requests.get('https://news.ycombinator.com/news')
-# print(res.text)
-soup= BeautifulSoup(res.text, 'html.parser')
-#print(soup)
-#print(soup.body)
+import pprint
 
-#finds all tags
-#print(soup.find_all('a'))
+i=0
+while i<4:
+    i+=1
 
-#finds the first tag
-#print(soup.find('a'))
+    res =requests.get('https://news.ycombinator.com/news?p='+str(i))
+    # print(res.text)
+    soup= BeautifulSoup(res.text, 'html.parser')
+    #print(soup)
+    #print(soup.body)
 
-links = (soup.select(".titlelink"))
-subtexts= soup.select(".subtext")
+    #finds all tags
+    #print(soup.find_all('a'))
+
+    #finds the first tag
+    #print(soup.find('a'))
+
+    links = (soup.select(".titlelink"))
+    subtexts= soup.select(".subtext")
+
+    def sorted_stories_by_votes(hnlist):
+        return sorted( hnlist, key= lambda k: k['votes'], reverse=True)
+
+    def create_custom_hn(links,subtexts):
+        hn=[]
+
+        for idx,item in enumerate(links):
+            title= links[idx].getText()
+            href=links[idx].get('href', None)
+            vote=subtexts[idx].select(".score")
+
+            if len(vote) :
+                points=int(vote[0].getText().replace("points", ""))
 
 
-def create_custom_hn(links,subtexts):
-    hn=[]
-
-    for idx,item in enumerate(links):
-        title= links[idx].getText()
-        href=links[idx].get('href', None)
-        vote=subtexts[idx].select(".score")
-
-        if len(vote) :
-            points=int(vote[0].getText().replace("points", ""))
-
-
-            hn.append({'title': title, "link": href, "votes":points})
-    return hn
-pprint(create_custom_hn(links,subtexts))
+                hn.append({'title': title, "link": href, "votes":points})
+        return sorted_stories_by_votes(hn)
+    pprint.pprint(create_custom_hn(links,subtexts))
